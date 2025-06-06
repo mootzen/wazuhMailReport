a7aa1f12bc3b0c8b8f99adc17d0e4039e07053d3
mootzen <info@mootzen.de>
2025-06-06 11:27:53 +0000

    Update logon_failure_report.sh

--- logon_failure_report.sh
+++ logon_failure_report.sh
@@ -101,3 +101,8 @@ echo "MIME-Version: 1.0"
 echo "Content-Type: text/html; charset=UTF-8"
 cat "$REPORT_FILE"
 ) | sendmail -f "$MAIL_FROM" "$MAIL_TO"
+if sendmail -f "$MAIL_FROM" "$MAIL_TO" < "$REPORT_FILE"; then
+    echo "[$$] Email sent successfully."
+else
+    echo "[$$] ERROR: Failed to send email."
+fi
